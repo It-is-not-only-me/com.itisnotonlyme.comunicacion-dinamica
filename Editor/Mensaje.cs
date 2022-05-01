772dc55ca6aa0d5817ee955bf044eeead716bce5
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2022-05-01 19:33:43 -0300

    Moviendo las clases especificas
    Antes las tenia en la interfaz ahora estan en sus propio archivo

---
+++ Editor/Mensaje.cs
@@ -0,0 +1,17 @@
+﻿namespace ItIsNotOnlyMe.ComunicacionDinamica
+{
+    public class Mensaje : IMensaje
+    {
+        private IImportancia _importancia;
+
+        public Mensaje(IImportancia importancia)
+        {
+            _importancia = importancia;
+        }
+
+        public bool MereceMandarMensaje(IImportancia importancia)
+        {
+            return importancia.EsMayorIgual(_importancia);
+        }
+    }
+}
---
+++ Editor/Persona.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+
+namespace ItIsNotOnlyMe.ComunicacionDinamica
+{
+    public class Persona : IPersona
+    {
+        private List<IVinculo> _vinculos;
+        private List<IMensaje> _mensajes;
+
+        public Persona()
+        {
+            _vinculos = new List<IVinculo>();
+            _mensajes = new List<IMensaje>();
+        }
+
+        public bool CrearVinculo(IVinculo vinculo)
+        {
+            IVinculo vinculoExistente = VinculoIgual(vinculo);
+            if (vinculoExistente != null)
+                return false;
+
+            _vinculos.Add(vinculo);
+            return true;
+        }
+
+        public bool ActualizarVinculo(IPersona persona, IImportancia importancia)
+        {
+            IVinculo vinculoExistente = VinculoIgual(persona);
+            if (vinculoExistente == null)
+                return false;
+
+            return vinculoExistente.Actualizar(importancia);
+        }
+
+        public void MandarMensaje(IMensaje mensaje)
+        {
+            if (_mensajes.Contains(mensaje))
+                return;
+            _mensajes.Add(mensaje);
+
+            foreach (IVinculo vinculo in _vinculos)
+                if (vinculo.MereceMandarMensaje(mensaje))
+                    vinculo.MandarMensaje(mensaje);
+        }
+
+        public IEnumerable<IMensaje> MensajesRecibidos()
+        {
+            foreach (IMensaje mensaje in _mensajes)
+                yield return mensaje;
+        }
+
+        private IVinculo VinculoIgual(IVinculo vinculo)
+        {
+            return _vinculos.Find(vinculado => vinculado.MismoVinculo(vinculo));
+        }
+
+        private IVinculo VinculoIgual(IPersona persona)
+        {
+            return _vinculos.Find(vinculado => vinculado.TieneVinculo(persona));
+        }
+    }
+}
